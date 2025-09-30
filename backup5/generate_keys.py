614b7a54ef76887cee44bd019d53a8a3b7de9667
Mohammad Jawad Waezi <mohammadwaezi2000@gmail.com>
2025-09-30 15:33:19 +0930

    s

---
+++ backup5/generate_keys.py
@@ -0,0 +1,27 @@
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
+
+# Generate RSA private key
+private_key = rsa.generate_private_key(public_exponent=65537, key_size=4096)
+
+
+# Save private key to PEM file
+with open("server_priv.pem", "wb") as f:
+    f.write(
+        private_key.private_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PrivateFormat.PKCS8,
+            encryption_algorithm=serialization.NoEncryption()
+        )
+    )
+
+# Save public key to PEM (optional, not required if you're using b64u strings)
+with open("server_pub.pem", "wb") as f:
+    f.write(
+        private_key.public_key().public_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PublicFormat.SubjectPublicKeyInfo
+        )
+    )
+
+print("✅ Keys generated: server_priv.pem and server_pub.pem")
